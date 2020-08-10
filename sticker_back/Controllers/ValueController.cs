90b268f72c33b1a1141e169749b8354581cfef90
Прокшин Илья Олегович <i.prokshin@modulbank.ru>
2020-08-10 15:26:01 +0500

    инициализация, реализация jwt auth

---
+++ sticker_back/Controllers/ValueController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sticker_back.Controllers
+{
+    [ApiController]
+    [Route("api/val")]
+    public class ValueController : Controller
+    {
+        [Authorize]
+        [Route("getlogin")]
+        public IActionResult GetLogin()
+        {
+            return Ok($"Ваш логин: {User.Identity.Name}");
+        }
+
+        [Authorize(Roles = "admin")]
+        [Route("getrole")]
+        public IActionResult GetRole()
+        {
+            return Ok("Ваша роль: администратор");
+        }
+    }
+}
---
+++ sticker_back/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sticker_back.Models
+{
+    public class User
+    {
+        //private Guid _id;
+        //private string firstName { get; set; }
+        //private string lastName { get; set; }
+        //private string login { get; set; }
+        //private string password { get; }
+        //private DateTime registrationDate { get; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+    }
+}
